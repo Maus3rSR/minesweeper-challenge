8dd0553a84a55667a131feede84ed446b6da8687
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-04-01 11:37:13 +0200

    CHORE: add example

---
+++ src/2025_04_01_test.spec.ts
@@ -0,0 +1,16 @@
+/**
+ * Scenario 1 : Mine field with 0 mine must resolve field with 0
+ *
+ * Given a mine field with 0 mines
+ * ......
+ * ......
+ * ......
+ *
+ * When I solve the mine field
+ *
+ * Then I receive the following resolve
+ *
+ * 000000
+ * 000000
+ * 000000
+ */
